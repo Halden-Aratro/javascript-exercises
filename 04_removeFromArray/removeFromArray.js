5d9ecc439e8b5c8ee04157a985d01be83efeb782
Halden-Aratro <marianne.bousquet1@gmail.com>
2022-03-22 21:00:46 +0100

    attempted JS loops ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    toRemove = toRemove.toString();
+    for (i = 0; i < array.length; i++) {
+        array = [1,2,3,4];
+        let slicedArray;
+/*         if (array[i] === toRemove) {
+            slicedArray = array.splice(i, 1);
+        } */
+        for (j = 0; j < toRemove.length; j++); {
+            if (array[i] === toRemove[j]) {
+                slicedArray = array.splice(i, 1);
+            }
+        }
 
+    }
+    return array;
+    
 };
 
 // Do not edit below this line
