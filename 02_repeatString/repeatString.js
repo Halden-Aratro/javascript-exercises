390a6a2d1ced9021014c6992b2b77649b1622208
Halden-Aratro <marianne.bousquet1@gmail.com>
2022-03-22 21:01:57 +0100

    done JS loops ex 1 to 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    //var to hold the string
+    let result = "";
+    //loop that repeats string n times + adds string to result on each loop
+    for (i = 1; i <= num; i++) {
+            result += string; 
+    } 
+    return result;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //take the string and parse it into an array
+    const splitString = Array.from(string);
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
